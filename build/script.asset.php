a29983e40878075d446f2d70ea67ab8b55072c95
devMiguelCarrero <developermiguelcarrero@gmail.com>
2022-08-18 18:04:37 -0500

    update section container classes

--- build/script.asset.php
+++ build/script.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery', 'react', 'wp-element'), 'version' => 'de67bfb9e1c1332bf35e1eb28deced64');
+<?php return array('dependencies' => array('jquery', 'react', 'wp-element'), 'version' => 'cdaddb3b59db23c2522963daf938816d');
