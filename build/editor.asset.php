b2126d99d359d9ff6bb2a91e5a675f62b1a1a779
devMiguelCarrero <developermiguelcarrero@gmail.com>
2024-01-18 15:01:02 -0500

    remove unused build folder

--- build/editor.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '602f122871c752113a40d574029aa6ac');
