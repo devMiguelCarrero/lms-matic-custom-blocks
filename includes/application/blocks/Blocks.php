314212445bea6ecd258832ac2dc6ed924ac85beb
devMiguelCarrero <developermiguelcarrero@gmail.com>
2024-01-21 17:06:26 -0500

    fix features blocks issue
    correct the name of the feature block in innerBlocks

--- includes/application/blocks/Blocks.php
+++ includes/application/blocks/Blocks.php
@@ -20,8 +20,8 @@ class LMSCB_Blocks
 		register_block_type_from_metadata(LMSCB_BLOCKS_PATH . 'highlighter-arrow');
 		register_block_type_from_metadata(LMSCB_BLOCKS_PATH . 'review-form');
 		register_block_type_from_metadata(LMSCB_BLOCKS_PATH . 'review-form-options');
-		register_block_type_from_metadata(LMSCB_BLOCKS_PATH . 'features');
 		register_block_type_from_metadata(LMSCB_BLOCKS_PATH . 'features/feature');
+		register_block_type_from_metadata(LMSCB_BLOCKS_PATH . 'features');
 		register_block_type_from_metadata(LMSCB_BLOCKS_PATH . 'add-to-cart-button');
 		register_block_type_from_metadata(LMSCB_BLOCKS_PATH . 'ajax-comments-block');
 	}
