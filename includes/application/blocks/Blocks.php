cfdfc87ce5879d18de38ebf34ffa6dd4bed1d4c1
devMiguelCarrero <developermiguelcarrero@gmail.com>
2024-01-22 20:25:53 -0500

    add pricing-tables custom block

--- includes/application/blocks/Blocks.php
+++ includes/application/blocks/Blocks.php
@@ -24,6 +24,7 @@ class LMSCB_Blocks
 		register_block_type_from_metadata(LMSCB_BLOCKS_PATH . 'features');
 		register_block_type_from_metadata(LMSCB_BLOCKS_PATH . 'add-to-cart-button');
 		register_block_type_from_metadata(LMSCB_BLOCKS_PATH . 'ajax-comments-block');
+		register_block_type_from_metadata(LMSCB_BLOCKS_PATH . 'pricing-tables');
 	}
 
 	public static function instance()
