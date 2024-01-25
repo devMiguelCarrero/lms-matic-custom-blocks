1ed40350b429a7a7743417ccef14c9e0373d0c84
devMiguelCarrero <developermiguelcarrero@gmail.com>
2024-01-25 10:27:20 -0500

    update pricing tables main functionality
    update styles, add pricing-tab-payment, add pricing-tab main attributes

--- src/blocks/pricing-tables/pricing-tab/edit.js
+++ src/blocks/pricing-tables/pricing-tab/edit.js
@@ -3,14 +3,21 @@ import {
 	InnerBlocks,
 	InspectorControls,
 } from '@wordpress/block-editor';
-import { PanelBody, TextControl } from '@wordpress/components';
+import {
+	PanelBody,
+	TextControl,
+	ColorPalette,
+	TextareaControl,
+} from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
-import { textDomain } from '../../../block-data/block-data';
+import { textDomain, Colors } from '../../../block-data/block-data';
 import classNames from 'classnames';
+import { formatearPrecio } from '../../../shared/util/numberFormat';
 
 const EditPricingTab = (props) => {
 	const { attributes, setAttributes } = props;
-	const { className, price } = attributes;
+	const { className, title, price, description, color, paymentInfo } =
+		attributes;
 
 	const classes = classNames(className, {
 		'lms-pricing-tabs__tab': true,
@@ -24,10 +31,25 @@ const EditPricingTab = (props) => {
 		setAttributes({ price: newPrice });
 	};
 
+	const onChangeColor = (newColor) => {
+		setAttributes({ color: newColor });
+	};
+
+	const onChangeTitle = (newTitle) => {
+		setAttributes({ title: newTitle });
+	};
+
+	const onChangeDescription = (newDescription) => {
+		setAttributes({ description: newDescription });
+	};
+
+	const onChangePaymentInfo = (newInfo) => {
+		setAttributes({ paymentInfo: newInfo });
+	};
+
 	const TEMPLATE = [
 		[
 			'core/list',
-			{},
 			[
 				['core/list-item', { placeholder: __('Item 1', textDomain) }],
 				['core/list-item', { placeholder: __('Item 2', textDomain) }],
@@ -41,20 +63,60 @@ const EditPricingTab = (props) => {
 			<InspectorControls>
 				<PanelBody>
 					<TextControl
-						label={__('Price', 'team-members')}
+						label={__('Title', 'lms-matic-custom-blocks')}
+						onChange={onChangeTitle}
+						value={title}
+					/>
+					<TextControl
+						label={__('Description', 'lms-matic-custom-blocks')}
+						onChange={onChangeDescription}
+						value={description}
+					/>
+					<TextControl
+						label={__('Price', 'lms-matic-custom-blocks')}
 						onChange={onChangePrice}
 						value={price}
 					/>
+					<TextareaControl
+						label={__('Payment Info', 'lms-matic-custom-blocks')}
+						onChange={onChangePaymentInfo}
+						value={paymentInfo}
+					/>
+				</PanelBody>
+				<PanelBody>
+					<ColorPalette
+						label={__('Background', 'team-members')}
+						value={color}
+						disableCustomColors={false}
+						colors={Object.keys(Colors).map((item) => ({
+							name: item,
+							color: Colors[item],
+						}))}
+						onChange={onChangeColor}
+					/>
 				</PanelBody>
 			</InspectorControls>
+			<div
+				style={{ backgroundColor: color }}
+				className="lms-pricing-tabs__title"
+			>
+				<h4 className="lms-pricing-heading">{title}</h4>
+				<small className="lms-pricing-description">
+					({description})
+				</small>
+			</div>
+			<InnerBlocks template={TEMPLATE} templateLock="all" />
 			<p
+				style={{ color }}
 				key="lms-pricing-tab-price"
 				className="lms-pricing-tabs__price"
 				formattingControl={['bold', 'italic']}
 			>
-				{price}
+				{formatearPrecio(price)}
 			</p>
-			<InnerBlocks template={TEMPLATE} templateLock="all" />
+			<div className="lms-pricing-tabs__payment">
+				<div dangerouslySetInnerHTML={{ __html: paymentInfo }} />
+			</div>
 		</div>
 	);
 };
--- src/blocks/pricing-tables/pricing-tab/save.js
+++ src/blocks/pricing-tables/pricing-tab/save.js
@@ -1,8 +1,10 @@
 import { useBlockProps, InnerBlocks } from '@wordpress/block-editor';
 import classNames from 'classnames';
+import { formatearPrecio } from '../../../shared/util/numberFormat';
 
 export default function SavePricingTab({ attributes }) {
-	const { className, price } = attributes;
+	const { className, price, color, description, title, paymentInfo } =
+		attributes;
 
 	const classes = classNames(className, {
 		'lms-pricing-tabs__tab': true,
@@ -14,8 +16,22 @@ export default function SavePricingTab({ attributes }) {
 
 	return (
 		<div {...blockProps}>
-			<p className="lms-pricing-tabs__price">{price}</p>
+			<div
+				style={{ backgroundColor: color }}
+				className="lms-pricing-tabs__title"
+			>
+				<h4 className="lms-pricing-heading">{title}</h4>
+				<small className="lms-pricing-description">
+					({description})
+				</small>
+			</div>
 			<InnerBlocks.Content />
+			<p style={{ color }} className="lms-pricing-tabs__price">
+				{formatearPrecio(price)}
+			</p>
+			<div className="lms-pricing-tabs__payment">
+				<div dangerouslySetInnerHTML={{ __html: paymentInfo }} />
+			</div>
 		</div>
 	);
 }
