cfc0349ff3c97c4c715a693322af96e293c664e3
devMiguelCarrero <developermiguelcarrero@gmail.com>
2024-01-25 10:27:51 -0500

    fix data blockquotes

--- src/block-data/block-data.js
+++ src/block-data/block-data.js
@@ -2,7 +2,7 @@ export const textDomain = 'lms-matic-custom-blocks';
 export const Colors = {
 	main: '#3B2642',
 	secondary: '#F8B03D',
-	third: "#125667", 
+	third: '#125667',
 	black: '#1C1C1B',
 	white: '#FFFFFF',
 	fail: '#FF0033',
