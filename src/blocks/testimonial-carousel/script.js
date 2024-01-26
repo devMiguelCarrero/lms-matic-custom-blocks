3b04fa6b83c200145a5000d8f4c91988a176e73d
devMiguelCarrero <developermiguelcarrero@gmail.com>
2024-01-26 15:01:01 -0500

    remove jQuery dependency from testimonial carousel

--- src/blocks/testimonial-carousel/script.js
+++ src/blocks/testimonial-carousel/script.js
@@ -2,21 +2,22 @@ import './style.scss';
 
 import { render, createPortal } from '@wordpress/element';
 import App from './App.js';
-import $ from 'jquery';
 
 function renderApp(element) {
-	const dataPosts = $(element).data('posts');
+	const dataPosts = element.getAttribute('data-posts');
 	render(<App dataPosts={dataPosts} />, element);
 }
 
-function processNewCarouselElements() {
-	$('body .lms-post-carousel:not(.rendered)').each(function (index, element) {
-		$(element).addClass('rendered');
-		renderApp(element);
-	});
+function processNewElement(className) {
+	document
+		.querySelectorAll(`body .${className}:not(.rendered)`)
+		.forEach((element, index) => {
+			element.classList.add('rendered');
+			renderApp(element);
+		});
 }
 
-function observeDOMChanges() {
+function observeDOMChanges(className) {
 	const targetNode = document.body;
 	const config = { childList: true, subtree: true };
 
@@ -26,9 +27,9 @@ function observeDOMChanges() {
 				mutation.addedNodes.forEach((node) => {
 					if (
 						node.nodeType === Node.ELEMENT_NODE &&
-						$(node).find('.lms-post-carousel').length > 0
+						node.querySelectorAll(`.${className}`).length > 0
 					) {
-						processNewCarouselElements();
+						processNewElement(className);
 					}
 				});
 			}
@@ -38,5 +39,5 @@ function observeDOMChanges() {
 	observer.observe(targetNode, config);
 }
 
-processNewCarouselElements();
-observeDOMChanges();
+processNewElement('lms-post-carousel');
+observeDOMChanges('lms-post-carousel');
