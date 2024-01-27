fadb0180add220aafaaea3bc0a3fefc601d07de1
devMiguelCarrero <developermiguelcarrero@gmail.com>
2024-01-26 22:00:25 -0500

    add post-render script to accordion-tab block

--- src/blocks/accordion-tabs/script.js
+++ src/blocks/accordion-tabs/script.js
@@ -3,9 +3,8 @@ import { render } from '@wordpress/element';
 
 import App from './App';
 
-const tabBlocks = document.getElementsByClassName('lms-accordion-tabs');
+function renderApp(tabBlock) {
 
-for (let tabBlock of tabBlocks) {
 	let tabArray = [];
 	let tab = tabBlock.getElementsByClassName('lms-accordion-tabs__tab');
 
@@ -13,8 +12,45 @@ for (let tabBlock of tabBlocks) {
 		let title = subTab.getElementsByClassName('tab-title')[0];
 		let content = subTab.getElementsByClassName('tab-content')[0];
 
-		tabArray.push({ title: title.innerText, content: content.innerHTML });
+		tabArray.push({
+			title: title.innerText,
+			content: content.innerHTML,
+		});
 	}
 
 	render(<App tabInfo={tabArray} />, tabBlock);
 }
+
+function processNewElement(className) {
+	document
+		.querySelectorAll(`body .${className}:not(.rendered)`)
+		.forEach((element, index) => {
+			element.classList.add('rendered');
+			renderApp(element);
+		});
+}
+
+function observeDOMChanges(className) {
+	const targetNode = document.body;
+	const config = { childList: true, subtree: true };
+
+	const observer = new MutationObserver((mutationsList) => {
+		for (const mutation of mutationsList) {
+			if (mutation.type === 'childList') {
+				mutation.addedNodes.forEach((node) => {
+					if (
+						node.nodeType === Node.ELEMENT_NODE &&
+						node.querySelectorAll(`.${className}`).length > 0
+					) {
+						processNewElement(className);
+					}
+				});
+			}
+		}
+	});
+
+	observer.observe(targetNode, config);
+}
+
+processNewElement('lms-accordion-tabs');
+observeDOMChanges('lms-accordion-tabs');
