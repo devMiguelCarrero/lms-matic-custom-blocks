c26799d566f54c7d28ef3e2c185b98f116947c8f
devMiguelCarrero <developermiguelcarrero@gmail.com>
2024-01-26 22:00:00 -0500

    update DisplayButton styles
    change button color & lint script

--- src/components/buttons/DisplayButton/DisplayButton.js
+++ src/components/buttons/DisplayButton/DisplayButton.js
@@ -3,27 +3,27 @@ import classes from './display-button.module.scss';
 import cx from 'classnames';
 
 const DisplayButton = ({
-  className = null,
-  disabled = false,
-  active = false,
-  size = 'small',
-  ...props
+	className = null,
+	disabled = false,
+	active = false,
+	size = 'small',
+	...props
 }) => {
-  const classNames = cx(classes['display-button'], {
-    [className]: className,
-    [classes.active]: active,
-    [classes[`display-button--size-${size}`]]: size,
-  });
+	const classNames = cx(classes['display-button'], {
+		[className]: className,
+		[classes.active]: active,
+		[classes[`display-button--size-${size}`]]: size,
+	});
 
-  return (
-    <button
-      {...props}
-      type={props.type}
-      className={classNames}
-      disabled={disabled}
-      onClick={props.onClick}
-    ></button>
-  );
+	return (
+		<button
+			{...props}
+			type={props.type}
+			className={classNames}
+			disabled={disabled}
+			onClick={props.onClick}
+		></button>
+	);
 };
 
 export default DisplayButton;
